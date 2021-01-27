fa69831b5df1b2bb71ba94e3e8b9499b233b6608
Domenic Bianchi <domenic@oncallhealth.com>
2021-01-26 22:24:57 -0500

    Save

--- Package.swift
+++ Package.swift
@@ -14,12 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MotionInterchange",
-            path: "src",
-            sources: [
-                "*.{h,m,mm}",
-                "private/*.{h,m,mm}"
-            ],
-            publicHeadersPath: "*.h"),
+            path: "src")
     ]
 )
 
