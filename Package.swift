2c5bcce873d76809dc9eb2a93c124ce0bc43485a
Domenic Bianchi <domenic@oncallhealth.com>
2021-01-26 22:03:29 -0500

    Save

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Motion Interchange",
+    platforms: [ .iOS(.v9) ],
+    products: [
+        .library(name: "MotionInterchange", targets: ["MotionInterchange-Objc"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MotionInterchange-Objc",
+            path: "src",
+            sources: [
+                "src/*.{h,m,mm}",
+                "src/private/*.{h,m,mm}"
+            ],
+            publicHeadersPath: "src/*.h"),
+    ]
+)
+
